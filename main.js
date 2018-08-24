2ccf5781deebdfc68af3a16eb4363cafafb8e3ab
Daniel Munoz <daniel.munozdev@gmail.com>
2018-08-23 19:32:52 -0700

    add starter files

---
+++ main.js
@@ -0,0 +1,9 @@
+// Factorial
+// A factorial is an operator that multiplies a number by all of the positive integers less than that number.
+// For example, 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// By definition, 0! = 1.
+
+// Make a web page to compute factorials.
+
+
